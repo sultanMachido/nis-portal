ef1fffc79a16ae7527061574f40ce292b3ac9e28
Adesina Opeoluwa Martins <opehardesina@gmail.com>
2021-06-30 16:06:25 +0100

    edit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Cover from './containers/Cover';
+
 import {BrowserRouter} from 'react-router-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
